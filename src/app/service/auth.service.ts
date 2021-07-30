b447607beba13f5164b86b67b473c0f8a41d577c
Pamella Santos <pamellaolys@gmail.com>
2021-07-29 22:44:04 -0300

    Consumindo API - model service e inicio

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(usuarioLogin: UsuarioLogin): Observable <UsuarioLogin>{
+return this.http.post<UsuarioLogin>('https://pampamblog.herokuapp.com/usuarios/logar', usuarioLogin)
+  }
+
+  cadastrar (usuario: Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>('https://pampamblog.herokuapp.com/usuarios/cadastrar', usuario)
+
+  }
+
+}
