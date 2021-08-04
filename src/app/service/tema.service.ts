132d99fde34aaac3062cf745507bdb599cc7286c
Pamella Santos <pamellaolys@gmail.com>
2021-08-04 19:55:46 -0300

    Crud completo de Tema

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]> ('https://pampamblog.herokuapp.com/temas/listar', this.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://pampamblog.herokuapp.com/temas/${id}` , this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema> ('https://pampamblog.herokuapp.com/temas', tema ,this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema> ('https://pampamblog.herokuapp.com/temas', tema ,this.token)
+  }
+
+
+  deleteTema(id: number){
+    return this.http.delete(`https://pampamblog.herokuapp.com/temas/${id}` , this.token)
+  }
+
+
+
+}
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,8 +11,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -19,6 +25,26 @@ export class TemaComponent implements OnInit {
       alert('Sua sessão expirou, faça login novamente')
       this.router.navigate(['/entrar'])
     }
+
+
+    this.findAllTemas()
+  }
+
+
+  // toda vez que iniciar minha pagina automaticamente esse metodo busca todos os temas e os lista
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) =>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
